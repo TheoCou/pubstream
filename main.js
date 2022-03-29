92a143c38617d2d2d60eea4fe80fa439851ecc55
Théo Courson <theocourson@gmail.com>
2022-03-29 16:27:33 +0200

    update 29/03/2022

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    console.log(navigator.userAgent);
+  
+    let el, modal, closed, open_modal, closed_all, footer_right;
+  
+    el = document.querySelectorAll(".grid-picture-large li");
+    modal = document.querySelector(".parent-modale");
+    closed = document.querySelector(".modale button");
+    closed_all = document.querySelector(".modale img");
+    footer_right = document.querySelector("footer p")
+})
