1224ad01525ba05d3f118984745e16c7c5fa27e7
Théo Courson <theocourson@gmail.com>
2022-04-09 15:50:44 +0200

    update 09/04/2022

--- form.js
+++ form.js
@@ -22,3 +22,43 @@ document.addEventListener("DOMContentLoaded", (e) => {
     }
 
 })
+window.addEventListener("load", () => {
+    const user = {
+        first_name: "Tony",
+        last_name: "Montana",
+        age: 35,
+        taille: 170
+    }
+    console.table(user)
+    const new_user = Object.create(user)
+    new_user.first_name = "Bruce"
+    new_user.last_name = "Wayne"
+    new_user.age = 40
+    new_user.taille = 190
+    const add_user = {
+        user,
+        new_user
+    }
+
+    for (rows in add_user.user) {
+        console.table(rows, add_user.user[rows])
+    }
+    //object & method
+    const persona = ({
+        nom: "Kent",
+        prenom: "Clark",
+        alias: "Superman",
+        age: 40,
+        set_out() {
+            console.log(`${this.nom} ${this.prenom} ${this.alias} ${this.age}`)
+        }
+    })
+    let print_out = persona.set_out()
+    console.log(print_out)
+    const new_persona = Object.create(persona)
+    new_persona.nom = "Allen"
+    new_persona.prenom = "Barry"
+    new_persona.alias = "Flash"
+    new_persona.age = 26
+
+    new_persona.set_out()
